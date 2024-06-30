ffc34666ed3464a1bf2c154ee33d773610c1f61a
Wellington <tonmelo@hotmail.com>
2024-06-30 17:36:02 -0300

    CRUD CREATE - controller - ligando a servlet a javabeans

--- src/main/java/controller/Controller.java
+++ src/main/java/controller/Controller.java
@@ -7,11 +7,13 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import model.DAO;
+import model.JavaBeans;
 
-@WebServlet(urlPatterns = { "/Controller", "/main" })
+@WebServlet(urlPatterns = { "/Controller", "/main", "/insert" })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	DAO dao = new DAO();
+	JavaBeans contato = new JavaBeans();
 
 	public Controller() {
 		super();
@@ -24,8 +26,10 @@ public class Controller extends HttpServlet {
 		System.out.println(action);
 		if (action.equals("/main")) {
 			contatos(request, response);
+		} else if (action.equals("/insert")) {
+			novoContato(request, response);
 		} else {
-
+			response.sendRedirect("index.html");
 		}
 		// response.getWriter().append("Served at: ").append(request.getContextPath());
 		// teste de conexão
@@ -35,6 +39,17 @@ public class Controller extends HttpServlet {
 	protected void contatos(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		response.sendRedirect("agenda.jsp");
+	}
+
+	protected void novoContato(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+		//teste de recebimento de formulário
+		//System.out.println(request.getParameter("nome"));
+		//System.out.println(request.getParameter("fone"));
+		//System.out.println(request.getParameter("email"));
+		contato.setNome(request.getParameter("nome"));
+		contato.setFone(request.getParameter("fone"));
+		contato.setEmail(request.getParameter("mail"));
 	}
 
 }
